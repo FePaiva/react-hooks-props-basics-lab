12566f66ea06a6436f1bf689880b9986e805f94e
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-13 20:34:13 -0800

    Assignment complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,41 @@
+// import React from "react";
+
+// function About(props) {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       {props.bio ? <p>{props.bio}</p> : null}
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       {/* add your <Links /> component here */}
+//       <a href={props.links.Github}></a>
+//       <a href={props.links.linkedin}></a>
+      
+//     </div>
+//   );
+// }
+
+
+// export default About;
+
 import React from "react";
 
-function About() {
+function About(prop) {
+  const bioText = prop.bio
+
+  function bio(){
+    if (bioText) {
+    return <p>{prop.bio}</p>
+    }
+  }
+
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2> 
+      {bio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={prop.github}>{prop.github}</a>
+      <a href={prop.linkedin}>{prop.linkedin}</a>
     </div>
   );
 }
